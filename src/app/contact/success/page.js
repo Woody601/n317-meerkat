dc52b944aa17709d46ed31fb6491deeee00db0fd
Woody601 <mychalwood@gmail.com>
2024-10-22 19:04:28 -0400

    Added the Contact page and the styling for it.

---
+++ src/app/contact/success/page.js
@@ -0,0 +1,11 @@
+export default function Success() {
+  return (
+    <>
+      <p>
+        Thank you for submitting the form! We will get back with you as soon as
+        possible!
+      </p>
+      <p>Note: this form does not actually submit anything at the moment.</p>
+    </>
+  );
+}
