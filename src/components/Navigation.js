6cbcaeb5d9caf88825ca3438282b45e3159ad5df
bakejona <jbakerbba2@gmail.com>
2024-10-22 17:18:21 -0400

    Added title to naviagtion

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,12 +4,15 @@ import styles from './Navigation.module.css';
 export default function Navigation() {
   return (
     <nav className={styles.nav}>
-      <ul>
-        <li><Link href="/">Home</Link></li>
-        <li><Link href="/about">About</Link></li>
-        <li><Link href="/gallery">Gallery</Link></li>
-        <li><Link href="/contact">Contact</Link></li>
-      </ul>
+      <div className={styles.container}>
+        <h2 className={styles.title}>Meerkat Conservation Group</h2>
+        <ul>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/about">About</Link></li>
+          <li><Link href="/gallery">Gallery</Link></li>
+          <li><Link href="/contact">Contact</Link></li>
+        </ul>
+      </div>
     </nav>
   );
 }
