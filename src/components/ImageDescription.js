ac64f0d5f15eaee3e361dd323020f70b0ee0608a
Woody601 <mychalwood@gmail.com>
2024-10-22 22:31:23 -0400

    Removed the changes I added to try to troubleshoot the problems of not deploying.

--- src/components/ImageDescription.js
+++
@@ -1,9 +0,0 @@
-import styles from './ImageDescription.module.css'
-export default function ImageDescription ({ image, description }) {
-  return (
-    <div className={styles.container}>
-      <img src={image} />
-      <p className={styles.description}>{description}</p>
-    </div>
-  );
-};
